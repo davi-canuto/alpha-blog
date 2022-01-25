f266fd1d049b74a75d6041bc454ee7c9133f1974
Davi Alessandro <davi@DESKTOP-DKC0VNU.localdomain>
2022-01-25 15:31:16 -0200

    add users index

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   resources :articles
   get 'signup', to: 'users#new'
-  resources :users, expect: [:new, :create]
+  resources :users, expect: [:new, :create, :index]
 end
