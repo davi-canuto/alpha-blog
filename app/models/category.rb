86f5a087b9223879d4696d3ff67aa96b04ab7876
davi-canuto <davicanutogregorio@gmail.com>
2022-05-08 10:09:58 -0300

    validations using unit tests

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
-  
+  validates :name, presence: true, uniqueness: { case_sensitive: false },
+            length: {minimum:3, maximum:24}
 end
--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,9 +1,37 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
-  test 'Category shoud be valid' do
+
+  def setup
     @category = Category.new(name: "Sports")
+  end
+
+  test 'Category shoud be valid' do
     assert @category.valid?
   end
 
+  test 'name should be present' do
+    @category.name = " "
+    assert_not @category.valid?
+  end
+
+  test 'name should be uniq' do
+    @category.save
+    @category2 = Category.new(name: "Sports")
+
+    assert_not @category2.valid?
+  end
+
+  test 'name should not be too long' do
+    @category.name = "A" * 25
+
+    assert_not @category.valid?
+
+  end
+
+  test 'name should not be too short' do
+    @category.name = "aa"
+
+    assert_not @category.valid?
+  end
 end
